7710039a46ed7ca96ce14f53c3117b83262020ee
JesseChen <lkchan0719@gmail.com>
2017-12-01 15:59:37 +0800

    build a prototype

--- Readme.md
+++ Readme.md
@@ -3,3 +3,6 @@
 A tool write in go for pattern search. It will supports regex and normal seach
 
 
+## Progress
+
+This is a demo, and it's very slow. 
--- walkdir.go
+++ walkdir.go
@@ -5,6 +5,8 @@ import (
     "os"
     "sync"
     "runtime"
+    "bufio"
+    "strings"
 )
 
 var wg sync.WaitGroup
@@ -12,11 +14,11 @@ var semaphoreChan = make(chan struct{}, runtime.GOMAXPROCS(runtime.NumCPU()))
 
 func main(){
     wg.Add(1)
-    lsFiles(".")
+    lsFiles(".", os.Args[1])
     wg.Wait()
 }
 
-func lsFiles(dir string) {
+func lsFiles(dir string, pattern string) {
     semaphoreChan <- struct{} {}
     file, err := os.Open(dir)
     if err != nil {
@@ -32,13 +34,41 @@ func lsFiles(dir string) {
     for _, f := range files {
         if f.IsDir() {
             wg.Add(1)
-            go lsFiles(dir + "/" + f.Name())
+            go lsFiles(dir + "/" + f.Name(), pattern)
+        } else if f.Mode().IsRegular() {
+            SearchFile(dir + "/" + f.Name(), pattern)
         }
-        fmt.Println(dir + "/" + f.Name())
     }
 
     defer func() {
         <-semaphoreChan
         wg.Done()
     }()
+}
+
+func SearchFile(filename, pattern string) {
+    handler, err:= os.Open(filename)
+    if err != nil {
+        fmt.Println("error in open file", filename, err)
+    }
+
+    reader := bufio.NewReader(handler)
+    var lineno  uint32 = 1
+
+    for {
+        line, err := reader.ReadString('\n')
+       
+        if err != nil {
+            break
+        }
+        
+        lineno += 1
+
+        if strings.Contains(line, pattern) {
+            fmt.Printf("%s - %d: %s",filename, lineno, line)
+        }
+    }
+
+    handler.Close()
+    
 }
