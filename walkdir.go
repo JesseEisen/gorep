60317c4e34c868545bbb2baafc8a7ef9187d0322
JesseChen <lkchan0719@gmail.com>
2017-12-07 16:40:45 +0800

    skip elf file

--- walkdir.go
+++ walkdir.go
@@ -11,11 +11,14 @@ import (
 
 var wg sync.WaitGroup
 var semaphoreChan = make(chan struct{}, runtime.GOMAXPROCS(runtime.NumCPU()))
+var magicNumber = []byte{0x7F,0x45,0x4C,0x46}
 
 func main(){
+    fmt.Println("\n")
     wg.Add(1)
-    lsFiles(".", os.Args[1])
+    lsFiles(os.Args[1], os.Args[2])
     wg.Wait()
+    fmt.Println("\n")
 }
 
 func lsFiles(dir string, pattern string) {
@@ -57,13 +60,16 @@ func SearchFile(filename, pattern string) {
 
     for {
         line, err := reader.ReadString('\n')
-       
         if err != nil {
             break
         }
-        
-        lineno += 1
+        first4 := []rune(line)
+        if strings.Compare(string(first4[:4]), string(magicNumber)) == 0 {
+            break;
+        }
 
+        lineno += 1
+    
         if strings.Contains(line, pattern) {
             fmt.Printf("%s - %d: %s",filename, lineno, line)
         }
